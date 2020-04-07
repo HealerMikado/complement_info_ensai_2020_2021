c4faeee81bb2daa8cca8f91edf0c763c336e3b0c
healermikado <pepin.remi@gmail.com>
2020-04-07 13:27:18 +0200

    Correction ex3

--- python_rpg/business_objects/personnage/abstract_personnage.py
+++ python_rpg/business_objects/personnage/abstract_personnage.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 
+from business_objects.statistique import Statistique
+
 
 class AbstractPersonnage(ABC):
-    def __init__(self, phrase_attaque, phrase_defense):
+    def __init__(self, phrase_attaque, phrase_defense, force, agilite, magie,
+                 defense, point_de_vie):
         self.phrase_attaque = phrase_attaque
         self.phrase_defense = phrase_defense
+        self.statistique = Statistique(force, agilite, magie, defense,
+                                       point_de_vie)
 
     @abstractmethod  # décorateur qui définit une méthode comme abstraite
     def attaque(self):
---
+++ python_rpg/business_objects/statistique.py
@@ -0,0 +1,7 @@
+class Statistique:
+    def __init__(self, force, agilite, magie, defense, point_de_vie):
+        self.force = force
+        self.agilite = agilite
+        self.magie = magie
+        self.defense = defense
+        self.point_de_vie = point_de_vie
