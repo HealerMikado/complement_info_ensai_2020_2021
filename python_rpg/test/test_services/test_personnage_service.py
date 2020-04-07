91818860d4b886f92c19c990c3f45a27874d4404
healermikado <pepin.remi@gmail.com>
2020-04-07 13:08:57 +0200

    Correction ex2

--- python_rpg/test/test_services/test_personnage_service.py
+++ python_rpg/test/test_services/test_personnage_service.py
@@ -1,73 +1,42 @@
 from unittest import TestCase
 
-from business_objects.personnage.personnage import Personnage, MAGICIEN, GUERRIER, VOLEUR
+from business_objects.personnage.guerrier import Guerrier
+from business_objects.personnage.magicien import Magicien
+from business_objects.personnage.voleur import Voleur
 from services.personnage_service import PersonnageService
 
 
 class TestPersonnageService(TestCase):
     def test_attaque_magicien(self):
         #GIVEN un personnage de type magicien
-        magicien = Personnage(MAGICIEN)
+        magicien = Magicien()
         expected_output = "Lance une boule de feu"
 
         #WHEN
         actual_output = PersonnageService.attaque(magicien)
 
         #THEN
-        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_output, actual_output.phrase_attaque)
 
     def test_attaque_guerrier(self):
         #GIVEN un personnage de type guerrier
-        guerrier = Personnage(GUERRIER)
+        guerrier = Guerrier()
         expected_output = "Donne un coup d'épée"
 
         #WHEN
         actual_output = PersonnageService.attaque(guerrier)
 
         #THEN
-        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_output, actual_output.phrase_attaque)
 
     def test_attaque_voleur(self):
         #GIVEN un personnage de type voleur
-        voleur = Personnage(VOLEUR)
-        expected_output = "Danse la carioca"
+        voleur = Voleur()
+        expected_output = "Tire à l'arc"
 
         #WHEN
         actual_output = PersonnageService.attaque(voleur)
 
         #THEN
-        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_output, actual_output.phrase_attaque)
 
-
-    def test_defense_magicien(self):
-        # GIVEN un personnage de type magicien
-        magicien = Personnage(MAGICIEN)
-        expected_output = "Utilise une barrière magique"
-
-        # WHEN
-        actual_output = PersonnageService.defense(magicien)
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
-
-    def test_defense_guerrier(self):
-        # GIVEN un personnage de type magicien
-        guerrier = Personnage(GUERRIER)
-        expected_output = "Utilise une barrière magique"
-
-        # WHEN
-        actual_output = PersonnageService.defense(guerrier)
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
-
-    def test_defense_voleur(self):
-        # GIVEN un personnage de type voleur
-        voleur = Personnage(VOLEUR)
-        expected_output = "Tire à l'arc"
-
-        # WHEN
-        actual_output = PersonnageService.defense(voleur)
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
