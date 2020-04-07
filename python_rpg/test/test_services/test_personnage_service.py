6b06357873cc02973da1a68e9656b8724c258102
healermikado <pepin.remi@gmail.com>
2020-04-07 17:21:07 +0200

    code base ex5

--- python_rpg/test/test_services/test_personnage_service.py
+++ python_rpg/test/test_services/test_personnage_service.py
@@ -4,9 +4,6 @@ from business_objects.attaque_info import AttaqueInfo
 from business_objects.objet.arme.arc import Arc
 from business_objects.objet.arme.baton import Baton
 from business_objects.objet.arme.epee import Epee
-from business_objects.objet.armure.armure_legere import ArmureLegere
-from business_objects.objet.armure.armure_lourde import ArmureLourde
-from business_objects.objet.armure.robe import Robe
 from business_objects.personnage.guerrier import Guerrier
 from business_objects.personnage.magicien import Magicien
 from business_objects.personnage.voleur import Voleur
@@ -68,69 +65,6 @@ class TestPersonnageService(TestCase):
 
         # WHEN
         actual_output = PersonnageService.attaque(voleur).phrase_attaque
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
-
-    def test_defense_magicien_robe(self):
-        # GIVEN
-        expected_output = "Le tissus reforcé par magie protégea son porteur"
-        baton_en_bois = Baton(
-            "Baton en bois"
-            , 0, 0, 0, 0, 0
-            , ""
-            , 10)
-        magicien = Magicien(
-            baton_en_bois
-            , Robe("Robe", 0, 0, 0, 0, 0, expected_output, 0)
-            , 1, 1, 1, 1, 1)
-
-        info_attaque = AttaqueInfo("",0)
-
-        # WHEN
-        actual_output = PersonnageService.defense(magicien,info_attaque).phrase_defense
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
-
-    def test_defense_guerrier_harnois(self):
-        # GIVEN
-        expected_output = "L'imposante armure absorba l'attaque"
-        master_sword = Epee(
-            "Master sword"
-            , 0, 0, 0, 0, 0
-            , ""
-            , 750)
-        guerrier = Guerrier(
-            master_sword
-            , ArmureLourde("Harnois complet", 0, 0, 0, 0, 0, expected_output, 0)
-            , 1, 1, 1, 1, 1)
-
-        info_attaque = AttaqueInfo("",0)
-
-        # WHEN
-        actual_output = PersonnageService.defense(guerrier,info_attaque).phrase_defense
-
-        # THEN
-        self.assertEqual(expected_output, actual_output)
-
-    def test_defense_voleur_armure_de_cuir(self):
-        # GIVEN
-        expected_output = "Le cuir tint bon et limita les dégâts"
-        arc_de_lynel = Arc(
-            ""
-            , 0, 0, 0, 0, 0
-            , expected_output
-            , 10)
-        voleur = Voleur(
-            arc_de_lynel
-            , ArmureLegere("Armure de cuir", 0, 0, 0, 0, 0, expected_output, 0)
-            , 1, 1, 1, 1, 1)
-
-        info_attaque = AttaqueInfo("",0)
-
-        # WHEN
-        actual_output = PersonnageService.defense(voleur,info_attaque).phrase_defense
 
         # THEN
         self.assertEqual(expected_output, actual_output)
